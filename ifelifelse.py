64325cc2b80464d97134d233d530b208db83dbf1
HariSelva5 <hariselva2301@gmail.com>
2020-08-28 18:49:23 +0530

    ifelifelse program

---
+++ ifelifelse.py
@@ -0,0 +1,9 @@
+a=input("enter some value\n")
+b=input("enter some value\n")
+c=input("enter some value\n")
+if(a>b and a>c):
+    print("a is \"greater\"")
+elif(b>a and b>c):
+    print("b is \"greater\"")
+else:
+    print("c is \"greater\"")
