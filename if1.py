18c9a9bfa13a171b87accd833892d98b9dd72e9c
HariSelva5 <hariselva2301@gmail.com>
2020-08-28 18:44:22 +0530

    if 1

---
+++ if1.py
@@ -0,0 +1,11 @@
+import math
+x=[1,3,4]
+for i in x:
+    x.append(i)
+    print(i)
+
+"""a=(input("enter a value"))
+if(math.isinf(a)):
+    print("The value is infinite")
+else:
+    print ("it is {}".format(a))"""
