5aff22f7d0493f6863c726ed343552bff55d82e0
HariSelva5 <hariselva2301@gmail.com>
2020-09-11 12:29:32 +0530

    author names

--- exe2.py
+++ exe2.py
@@ -1,9 +1,18 @@
 import requests,json
 r=requests.get('https://api.quotable.io/quotes?tags=wisdom,famous-quotes')
-wisdom=r.json()
-print(wisdom)
-with open('wisdom.json', 'w') as f:
-    json.dump(wisdom, f)
-    print(len(wisdom["results"]))
-    
+gandhio=r.json()
+print(gandhio)
+with open('gandhio.json', 'w') as f:
+    json.dump(gandhio, f)
+resjson = r.json()
+icount = 0
+listAuthor=[]
+for i in resjson['results']:
+    icount+=1
+    listAuthor.append(i['author'])
+print(icount)
+listAuthor = list(dict.fromkeys(listAuthor))
+for a in listAuthor:
+    print(a)
+
     
